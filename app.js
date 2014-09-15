4d58620cb0a64f3b396cd64725286c6e62b28cbc
Diana Thayer <garbados@gmail.com>
2014-09-15 16:46:21 -0700

    polish auth

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ app.use(cookieParser());
 app.use(session({ secret: process.env.APP_SECRET }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(function (req, res, next) {
+  if (req.user) res.locals.user = req.user;
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', auth);
